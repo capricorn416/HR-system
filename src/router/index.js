387e6184da9fa6a3533d39b2089c7a41b81eca35
sunshine2233 <1599403803@qq.com>
2021-07-21 20:22:31 +0800

    [push]

--- src/router/index.js
+++ src/router/index.js
@@ -11,14 +11,10 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
+    path: '/aboutus',
+    name: 'AboutUs',
+    component: AboutUs,},
+    {
     path: '/introduction',
     name: 'Introduction',
     component: () => import(/* webpackChunkName: "about" */ '../views/Introduction.vue')
