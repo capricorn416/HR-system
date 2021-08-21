870e5b2e3303d5bf18b121856288113b4f5259da
chronos <1769712655@qq.com>
2021-08-21 08:25:48 +0800

    fix: request path and devproxy

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,25 @@ module.exports = {
             'views': '@/views',
         }
     }
-  }
+  },
+  // 反向代理
+  devServer: {
+    // http2:true,
+    https: false,
+    proxy: {
+      '/api': {
+        // 要访问的跨域的域名
+        target: 'https://www.pivotstudio.cn/', //'http://localhost:5001',
+        // target: 'https://limfx.pro',
+        ws: true, // 是否启用websockets
+        pathRewrite: {
+          '^/frontend': ''
+        },
+        secure: false, // 使用的是http协议则设置为false，https协议则设置为true
+        // 开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样客户端端和服务端进行数据的交互就不会有跨域问题
+        // changOrigin: true,
+        cookieDomainRewrite: "localhost"
+      },
+    }
+  },
 }
